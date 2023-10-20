6999496f1ba3e96afbd690e9fd5094da144e2259
imlyzh <enterindex@gmail.com>
2023-10-20 21:23:45 +0800

    complete struct ser/deser derive macro

--- src/lib.rs
+++ src/lib.rs
@@ -40,3 +40,48 @@ pub fn derive_ser(item: TokenStream) -> TokenStream {
         _ => panic!("not support union"),
     }
 }
+
+
+#[proc_macro_derive(RmpDeSer)]
+pub fn derive_deser(item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as DeriveInput);
+    let struct_name = input.ident.clone();
+    match input.data {
+        syn::Data::Struct(ds) => {
+            let body = ds
+                .fields
+                .iter()
+                .map(|f| {
+                    let ident = f.ident.clone().unwrap();
+                    quote!(value.#ident = limit_stream_runtime::utils::ls_read_value(buf)?;)
+                })
+                .reduce(|mut l, r| {
+                    l.extend(vec![r]);
+                    l
+                })
+                .unwrap();
+            // println!("{}", body.to_string());
+            let r = quote! {
+                impl limit_stream_runtime::Deser for #struct_name {
+                    type Res = Result<Self, ()>;
+                    fn deser(buf: &mut limit_stream_runtime::utils::Bytes) -> Result<Self, ()> {
+                        if limit_stream_runtime::utils::ls_read_array_len(buf)? != 3 {
+                            return Err(());
+                        }
+                        #[allow(invalid_value)]
+                        let mut value = unsafe { core::mem::MaybeUninit::<#struct_name>::uninit().assume_init() };
+                        #body
+                        Ok(value)
+                    }
+                }
+            };
+            // println!("{}", r.to_string());
+            return r.into();
+        }
+        syn::Data::Enum(_de) => {
+            // de.variants.into_iter().map(|x| x.)
+            todo!()
+        }
+        _ => panic!("not support union"),
+    }
+}
--- tests/test.rs
+++ tests/test.rs
@@ -1,9 +1,10 @@
-use limit_stream_derive::RmpSer;
+use limit_stream_derive::{RmpSer, RmpDeSer};
+use limit_stream_runtime::builtin_type::Uint;
 
-#[derive(RmpSer)]
+#[derive(RmpSer, RmpDeSer)]
 pub struct User {
     pub name: String,
-    pub age: limit_stream_runtime::builtin_type::Uint,
+    pub age: Uint,
     pub description: String,
 }
 
