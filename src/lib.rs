b4bac226bfc8bf73085590d125269b0bae860482
imlyzh <enterindex@gmail.com>
2023-10-20 22:23:09 +0800

    complete enum ser/deser derive macro

--- src/lib.rs
+++ src/lib.rs
@@ -20,27 +20,51 @@ pub fn derive_ser(item: TokenStream) -> TokenStream {
                     l
                 })
                 .unwrap();
-            // println!("{}", body.to_string());
+            let len = ds.fields.len();
             let r = quote! {
               impl limit_stream_runtime::Ser for #struct_name {
                 fn ser(&self, buf: &mut limit_stream_runtime::utils::ByteBuf) -> Result<(), ()> {
-                    limit_stream_runtime::utils::ls_write_array_len(buf, 3)?;
+                    limit_stream_runtime::utils::ls_write_array_len(buf, #len as u32)?;
                     #body
                     Ok(())
                 }
               }
             };
-            // println!("{}", r.to_string());
-            return r.into();
+            r.into()
         }
-        syn::Data::Enum(_de) => {
-            // de.variants.into_iter().map(|x| x.)
-            todo!()
+        syn::Data::Enum(de) => {
+            let body = de
+                .variants
+                .iter()
+                .map(|f| {
+                    let ident = &f.ident;
+                    let ident_str = ident.to_string();
+                    quote!(#struct_name::#ident(v) => {
+                        limit_stream_runtime::utils::ls_write_str(buf, #ident_str)?;
+                        v.ser(buf)?;
+                    })
+                })
+                .reduce(|mut l, r| {
+                    l.extend(vec![r]);
+                    l
+                })
+                .unwrap();
+            let r = quote! {
+                impl limit_stream_runtime::Ser for #struct_name {
+                    fn ser(&self, buf: &mut limit_stream_runtime::utils::ByteBuf) -> Result<(), ()> {
+                        limit_stream_runtime::utils::ls_write_array_len(buf, 2)?;
+                        match self {
+                            #body
+                        }
+                        Ok(())
+                    }
+                }
+            };
+            r.into()
         }
         _ => panic!("not support union"),
     }
 }
-
 
 #[proc_macro_derive(RmpDeSer)]
 pub fn derive_deser(item: TokenStream) -> TokenStream {
@@ -60,12 +84,12 @@ pub fn derive_deser(item: TokenStream) -> TokenStream {
                     l
                 })
                 .unwrap();
-            // println!("{}", body.to_string());
+            let len = ds.fields.len();
             let r = quote! {
                 impl limit_stream_runtime::Deser for #struct_name {
                     type Res = Result<Self, ()>;
                     fn deser(buf: &mut limit_stream_runtime::utils::Bytes) -> Result<Self, ()> {
-                        if limit_stream_runtime::utils::ls_read_array_len(buf)? != 3 {
+                        if limit_stream_runtime::utils::ls_read_array_len(buf)? != #len as u32 {
                             return Err(());
                         }
                         #[allow(invalid_value)]
@@ -75,12 +99,37 @@ pub fn derive_deser(item: TokenStream) -> TokenStream {
                     }
                 }
             };
-            // println!("{}", r.to_string());
-            return r.into();
+            r.into()
         }
-        syn::Data::Enum(_de) => {
-            // de.variants.into_iter().map(|x| x.)
-            todo!()
+        syn::Data::Enum(de) => {
+            let body = de
+                .variants
+                .iter()
+                .map(|f| {
+                    let ident = &f.ident;
+                    let ident_str = ident.to_string();
+                    quote!(#ident_str => Ok(#struct_name::#ident(limit_stream_runtime::utils::ls_read_value(buf)?)),)
+                })
+                .reduce(|mut l, r| {
+                    l.extend(vec![r]);
+                    l
+                })
+                .unwrap();
+            let r = quote! {
+                impl limit_stream_runtime::Deser for #struct_name {
+                    type Res = Result<Self, ()>;
+                    fn deser(buf: &mut limit_stream_runtime::utils::Bytes) -> Result<Self, ()> {
+                        if limit_stream_runtime::utils::ls_read_array_len(buf)? != 2 {
+                            return Err(());
+                        }
+                        match limit_stream_runtime::utils::ls_read_str(buf)?.as_str() {
+                            #body
+                            _ => return Err(()),
+                        }
+                    }
+                }
+            };
+            r.into()
         }
         _ => panic!("not support union"),
     }
--- tests/test.rs
+++ tests/test.rs
@@ -1,5 +1,6 @@
-use limit_stream_derive::{RmpSer, RmpDeSer};
+use limit_stream_derive::{RmpDeSer, RmpSer};
 use limit_stream_runtime::builtin_type::Uint;
+use limit_stream_runtime::{Deser, Ser};
 
 #[derive(RmpSer, RmpDeSer)]
 pub struct User {
@@ -8,8 +9,15 @@ pub struct User {
     pub description: String,
 }
 
+#[derive(RmpSer, RmpDeSer)]
+pub enum UserForm {
+    User(User),
+    Id(Uint),
+}
+
 #[test]
 fn test() {
+    UserForm::Id(114);
     // let src: TokenStream = "".parse().unwrap();
     // derive_ser(src);
 }
